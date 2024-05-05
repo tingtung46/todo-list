5e94da1ee1e3fd1a1d5472c7bcc336480cb61423
root <132725436+tingtung46@users.noreply.github.com>
2024-05-06 06:39:00 +0700

    Add todo object skeleton

---
+++ src/modules/todo.js
@@ -0,0 +1,11 @@
+class Todo {
+    constructor(title, description, dueDate, priority, done) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.done = done;
+    };
+};
+
+export default Todo;

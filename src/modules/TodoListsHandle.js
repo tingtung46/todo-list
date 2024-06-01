ce554e950e8867c6d9d1f0b5628bfc9fda7e291c
root <132725436+tingtung46@users.noreply.github.com>
2024-06-01 10:02:16 +0700

    Fix typo and remove todo done

--- src/modules/TodoListsHandle.js
+++ src/modules/TodoListsHandle.js
@@ -3,8 +3,8 @@ import Todo from './todo';
 let todoLists = [];
 
 class TodoListsHandle {
-    static addTodoList(project, title, description, dueDate, priority, done) {
-        const todoList = new Todo(title, description, dueDate, priority, done);
+    static addTodoList(project, title, description, dueDate, priority) {
+        const todoList = new Todo(title, description, dueDate, priority);
         project.todoLists.push(todoList);
     };
 
@@ -12,7 +12,7 @@ class TodoListsHandle {
         project.todoLists.find((todoList) => todoList.title === title);
     };
 
-    static removeTodoLists(project, title) {
+    static removeTodoList(project, title) {
         const todoList = TodoListsHandle.findTodoList(project, title);
         project.todoLists.splice(project.todoLists.indexOf(todoList), 1);
     };
@@ -32,7 +32,6 @@ class TodoListsHandle {
         newDescription,
         newDueDate,
         newPriority,
-        newDone
     ) {
         const todoList = TodoListsHandle.findTodoList(project, title);
         if (todoList) {
@@ -41,16 +40,14 @@ class TodoListsHandle {
                 todoList.description = newDescription;
                 todoList.dueDate = newDueDate;
                 todoList.priority = newPriority;
-                todoList.done = newDone;
             } else {
-                this.removeTodoLists(project, title);
-                this.addTodoList(
+                TodoListsHandle.removeTodoList(project, title);
+                TodoListsHandle.addTodoList(
                     newProject,
                     newTitle,
                     newDescription,
                     newDueDate,
                     newPriority,
-                    newDone
                 );
             };
         };
